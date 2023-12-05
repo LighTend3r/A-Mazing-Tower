1727474cf81adeb59028ae8db5369d266e1f46c7
LighTender - Dev <87587438+LighTend3r@users.noreply.github.com>
2023-12-05 16:16:29 +0100

    typage Maze

--- src/server/maze/Maze.py
+++ src/server/maze/Maze.py
@@ -5,7 +5,7 @@ class Maze:
     def __init__(self, row:int, column:int, wall: int | str, floor: int | str, coin: int | str):
         self.__column: int = column
         self.__row: int = row
-        self.__grid: List[List[int]] = [[wall for _ in range(column)] for _ in range(row)]
+        self.__grid: List[List[int|str]] = [[wall for _ in range(column)] for _ in range(row)]
         self.__wall: int | str = wall
         self.__floor: int | str = floor
         self.__coin: int | str = coin
@@ -20,7 +20,7 @@ class Maze:
         """
         return self.__row
 
-    def get_grid(self) -> List[List[int]]:
+    def get_grid(self) -> List[List[int|str]]:
         """Récupère la grille de labyrinthe
         """
         return self.__grid
@@ -40,12 +40,12 @@ class Maze:
         """
         return self.__coin
 
-    def set_tile(self, x:int, y:int, tile:int) -> None:
+    def set_tile(self, x:int, y:int, tile:int|str) -> None:
         """Modifie une case de la grille
         """
         self.__grid[x][y] = tile
 
-    def get_tile(self, x:int, y:int) -> int:
+    def get_tile(self, x:int, y:int) -> int|str:
         """Récupère une case de la grille
         """
         return self.__grid[x][y]
